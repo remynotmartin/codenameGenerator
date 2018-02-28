180fe8035cfee23881cd129a4678d3bdb4d99f08
remynotmartin <remy_dinh@yahoo.com>
2018-02-28 12:58:13 -0800

    Realized that I needed open and close parentheses for the file open checks lol, build script in the binary directory is working

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -5,21 +5,29 @@
 #include <fstream>
 #include <cstdlib>
 
-const std::string file1("prefixes.txt");
-const std::string file2("suffixes.txt");
+const std::string file1("../data/prefixes.txt");
+const std::string file2("../data/suffixes.txt");
 
 int main() {
+    
     std::ifstream prefixFile(file1);
-    if (!prefixFile)
+    if (!prefixFile) {
         std::cerr << "Could not open file '" << file1 << "'\n";
-        exit;
+        exit(EXIT_FAILURE);
+    }
     std::ifstream suffixFile(file2);
-    if (!suffixFile)
+    if (!suffixFile) {
         std::cerr << "Could not open file '" << file2 << "'\n";
-        exit;
+        exit(EXIT_FAILURE);
+    }
+    
     // There are 42 Prefixes and 38 Suffixes
-    int prefixCount = 0,
-        suffixCount = 0;
+    int prefixCount = 0;
+    int suffixCount = 0;
+
+    std::cout << "We counted " << prefixCount <<  " prefixes, commander.\n";
+    std::cout << "We counted " << suffixCount <<  " suffixes, commander.\n";
+
     /*
     std::srand(time(NULL));
     int prefixSeed = rand()%100 + 1;
