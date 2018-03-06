19a2f66060bfa23e375b163309018ecd661c1122
remynotmartin <remy_dinh@yahoo.com>
2018-03-06 11:51:42 -0800

    Counting function looks promising, data is being read into the buffer just fine!

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -10,11 +10,9 @@ const char* file2("../data/suffixes.txt");
 
 void checkFileOpen(const char* filename);
 void countEntries(const char* filename, int &counterVar, const char* typeRun); // There are 42 Prefixes and 38 Suffixes
-void populateArray();
+//void populateArray(const char* filename, int &counterVar, const char* typeRun, const char* arrayName);
 
 int main() {
-    checkFileOpen(file1);
-    checkFileOpen(file2);
     int prefixCount = 0;
     int suffixCount = 0;
     countEntries(file1, prefixCount, "prefixes");
@@ -23,14 +21,24 @@ int main() {
 }
 
 void checkFileOpen(const char* filename) {
-    std::ifstream fileOpen(filename);
-    if (!fileOpen) {
+    std::ifstream openFile(filename);
+    if (!openFile) {
         std::cerr << "Could not open file '" << filename << "', please check filename and path.\n";
         exit(EXIT_FAILURE);
     }
+    openFile.close();
 }
 
 void countEntries(const char* filename, int &wordCounter, const char* typeRun) {
-    std::cout << "Function successfully prototyped!\n";
-    std::cout << "We are currently looking at a file full of " << typeRun << ".\n";
+    checkFileOpen(filename);
+    std::cout << "We are currently looking at a file of " << typeRun << ".\n";
+    std::ifstream openFile(filename);
+    std::string buffer = ""; // initialize empty string
+    while (!openFile.eof()) {
+        openFile >> buffer;
+        if (openFile.eof())
+            break;
+        std::cout << buffer << '\n';
+        buffer.clear();
+    }
 }
