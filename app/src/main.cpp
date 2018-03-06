c0c1c4a5c9846e7e7f1810344a3d7f492f243537
remynotmartin <remy_dinh@yahoo.com>
2018-03-05 21:44:20 -0800

    relegated file open check to a separate function in main.cpp

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -5,34 +5,42 @@
 #include <fstream>
 #include <cstdlib>
 
-const std::string file1("../data/prefixes.txt");
-const std::string file2("../data/suffixes.txt");
+const char* file1("../data/prefixes.txt");
+const char* file2("../data/suffixes.txt");
+
+//void countEntries(std::ifstream openFile, int counter);
+void checkFileOpen(const char* filename);
 
 int main() {
-    
-    std::ifstream prefixFile(file1);
-    if (!prefixFile) {
-        std::cerr << "Could not open file '" << file1 << "'\n";
-        exit(EXIT_FAILURE);
-    }
-    std::ifstream suffixFile(file2);
-    if (!suffixFile) {
-        std::cerr << "Could not open file '" << file2 << "'\n";
-        exit(EXIT_FAILURE);
-    }
+    checkFileOpen(file1);
+    checkFileOpen(file2);
     
     // There are 42 Prefixes and 38 Suffixes
     int prefixCount = 0;
     int suffixCount = 0;
+    
+    //countEntries(prefixFile, prefixCount);
+    //countEntries(suffixFile, suffixCount);
 
     std::cout << "We counted " << prefixCount <<  " prefixes, commander.\n";
     std::cout << "We counted " << suffixCount <<  " suffixes, commander.\n";
 
-    /*
-    std::srand(time(NULL));
-    int prefixSeed = rand()%100 + 1;
-    int suffixSeed = rand()%100 + 1;
-    */
     return 0;
 
 }
+
+void checkFileOpen(const char* filename) {
+    std::ifstream fileOpen(filename);
+    if (!fileOpen) {
+        std::cerr << "Could not open file '" << filename << "', please check filename and path.\n";
+        exit(EXIT_FAILURE);
+    }
+}
+
+/*
+void countEntries(std::ifstream filename, int wordCounter) {
+
+    std::cout << "Function successfully prototyped! Awaiting further definition.\n";
+
+}
+*/
