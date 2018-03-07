ca14ec225b706042372fc01a058af86c1f558fa3
remynotmartin <remy_dinh@yahoo.com>
2018-03-06 22:59:50 -0800

    Got clearArray function to work for suffix and prefix arrays

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -11,13 +11,20 @@ const char* wordType2("suffixes");
 
 void checkFileOpen(const char* filename);
 void countEntries(const char* filename, int &counterVar, const char* typeRun);
-void clearBufferArray (char* arrayName, const int arraySize); 
-//void populateArray(const char* filename, int &counterVar, const char* typeRun, const char* arrayName);
+void clearArray (char* arrayName, const int arraySize); 
+void clearArray (char** arrayName, const int arraySize); 
+void populateArray(const char* filename, const int counterVar, const char* typeRun, const char* arrayName);
 
 int main() {
     int prefixCount = 0, suffixCount = 0;
     countEntries(file1, prefixCount, wordType1);
     countEntries(file2, suffixCount, wordType2); 
+    char* prefixArray[prefixCount];
+    char* suffixArray[suffixCount];
+    clearArray(prefixArray, prefixCount);
+    clearArray(suffixArray, suffixCount);
+    //populateArray(file1, prefixCount, wordType1, prefixArray);
+    //populateArray(file2, suffixCount, wordType2, suffixArray);
     return 0;
 }
 
@@ -35,20 +42,29 @@ void countEntries(const char* filename, int &wordCounter, const char* typeRun) {
     std::ifstream openFile(filename);
     int bufferSize = 16;
     char buffer[bufferSize];
-    clearBufferArray(buffer, bufferSize);
+    clearArray(buffer, bufferSize);
     while (!openFile.eof()) {
         openFile >> buffer;
         if (openFile.eof())
             break;
-        clearBufferArray(buffer, bufferSize);
+        clearArray(buffer, bufferSize);
         wordCounter++;
     }
     std::cout << "Commander, we counted " << wordCounter << ' ' << typeRun << ".\n";
     openFile.close();
 }
 
-void clearBufferArray (char* arrayName, const int arrayCount) {
+void clearArray (char* arrayName, const int arrayCount) {
     for (int i = 0; i > arrayCount; i++) {
         arrayName[i] = '\0'; // Each member is set to null
     }
+}
+
+void clearArray (char** arrayName, const int arrayCount) {
+    for (int i = 0; i > arrayCount; i++) {
+        arrayName[i] = '\0'; // Each member is set to null
+    }
+}
+
+void populateArray(const char* filename, const int counterVar, const char* typeRun, const char* arrayName) {
 }
