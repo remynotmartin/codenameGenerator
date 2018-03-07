af088579a9caf15b9c680ebb6896ed496d33de87
remynotmartin <remy_dinh@yahoo.com>
2018-03-06 21:21:58 -0800

    Added some more suffixes and prefixes

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <iomanip>
 #include <ctime>
-#include <string>
 #include <fstream>
 #include <cstdlib>
 
 const char* file1("../data/prefixes.txt");
 const char* file2("../data/suffixes.txt");
+const char* wordType1("prefixes");
+const char* wordType2("suffixes");
 
 void checkFileOpen(const char* filename);
-void countEntries(const char* filename, int &counterVar, const char* typeRun); // There are 42 Prefixes and 38 Suffixes
-void clearBufferArray (char* arrayName, const int arrayCount); 
+void countEntries(const char* filename, int &counterVar, const char* typeRun);
+void clearBufferArray (char* arrayName, const int arraySize); 
 //void populateArray(const char* filename, int &counterVar, const char* typeRun, const char* arrayName);
 
 int main() {
-    int prefixCount = 0;
-    int suffixCount = 0;
-    countEntries(file1, prefixCount, "prefixes");
-    countEntries(file2, suffixCount, "suffixes");
+    int prefixCount = 0, suffixCount = 0;
+    countEntries(file1, prefixCount, wordType1);
+    countEntries(file2, suffixCount, wordType2); 
     return 0;
 }
 
@@ -49,6 +49,6 @@ void countEntries(const char* filename, int &wordCounter, const char* typeRun) {
 
 void clearBufferArray (char* arrayName, const int arrayCount) {
     for (int i = 0; i > arrayCount; i++) {
-        arrayName[i] = 0;
+        arrayName[i] = '\0'; // Each member is set to null
     }
 }
